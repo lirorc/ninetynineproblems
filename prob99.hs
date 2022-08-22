4e05608caa6e6cbfa0168f466a69cdeb80786399
lirorc <74657909+lirorc@users.noreply.github.com>
2022-08-22 14:50:02 +0300

    Solve until problem 7

--- prob99.hs
+++ prob99.hs
@@ -4,8 +4,6 @@ cadr lst = (car (cdr lst))
 caddr lst = (car (cdr (cdr lst)))
 cddr lst = (cdr (cdr lst))
 
-nil lst = (cdr lst) == []
-
 findlast lst =
   if (cdr lst) == []
     then lst
@@ -16,7 +14,23 @@ findlast2 lst =
     then lst
     else (findlast2 (cdr lst))
 
+nth lst n =
+  if n == 1
+    then (car lst)
+    else nth (cdr lst) (n-1)
+
 elemcount lst =
-  if (nil (cdr lst))
-    then 1
+  if (null lst)
+    then 0
     else 1 + (elemcount (cdr lst))
+
+listreverse' l1 l2 c =
+  if c > 0
+    then
+      listreverse' (cdr l1) ((car l1):l2) (c-1)
+    else l2
+listreverse lst =
+  listreverse' lst [] (elemcount lst)
+
+palindrome lst =
+  lst == (listreverse lst)
