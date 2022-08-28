127e25122b651caae1bbdd3a5a6c164e7a3b245f
lirorc <74657909+lirorc@users.noreply.github.com>
2022-08-28 22:45:57 +0300

    Solve until problem 10

--- prob99.hs
+++ prob99.hs
@@ -1,18 +1,26 @@
 car lst = head lst
+caar lst = (car (car lst))
 cdr lst = tail lst
 cadr lst = (car (cdr lst))
 caddr lst = (car (cdr (cdr lst)))
 cddr lst = (cdr (cdr lst))
 
-findlast lst =
-  if (cdr lst) == []
-    then lst
-    else (findlast (cdr lst))
+-- findlast lst =
+--   if (cdr lst) == []
+--     then lst
+--     else (findlast (cdr lst))
+findlast [] = []
+findlast (x:[]) = x
+findlast (x:xs) = findlast xs
 
-findlast2 lst =
-  if (cddr lst) == []
-    then lst
-    else (findlast2 (cdr lst))
+-- findlast2 lst =
+--   if (cddr lst) == []
+--     then lst
+--     else (findlast2 (cdr lst))
+findlast2 [] = []
+findlast2 (x:[]) = [x]
+findlast2 (x1:x2:[]) = [x1,x2]
+findlast2 (x:xs) = findlast2 xs
 
 nth lst n =
   if n == 1
@@ -35,7 +43,6 @@ listreverse lst =
 palindrome lst =
   lst == (listreverse lst)
 
-
 -- Credit josuf107 @ r/haskellquestions
 -- Types means typing more, no?
 data Tree a = Tree [Tree a] | Atom a deriving Show
@@ -45,7 +52,17 @@ flatten (Atom a) = [a]
 -- eg. flatten (Tree [Atom 2, Atom 5, Tree [Atom 5, Atom 3]])
 
 compress' l1 l2
-  | l1 == [] = l2
+  | null l1 = l2
   | elem (car l1) l2 = compress' (cdr l1) l2
   | otherwise = compress' (cdr l1) (l2 ++ [car l1])
 compress lst = compress' lst []
+
+pack [] l2 = l2
+pack (x:xs) [] = pack xs [[x]]
+pack (x1:x1s) [(x2:x2s)] | x1 == x2 = pack x1s [(x1:x2:x2s)]
+pack (x:xs) l =
+  if x == (head (last l)) then
+    pack
+      xs
+      ((init l) ++ [(last l) ++ [x]])
+    else pack xs (l ++ [[x]])
